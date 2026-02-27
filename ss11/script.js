bc99ab9cc421e67f9afabce139f744fe42082473
tiennamz <ghastnam001@gmail.com>
2026-02-27 13:40:55 +0700

    update

--- ss11/script.js
+++ ss11/script.js
@@ -1,7 +1,7 @@
 let products = [
 { id: "P01", name: "Laptop MacBook Pro M3", price: 2000, category: "Laptop", inStock: true },
-{ id: "P02", name: "Chuột  không dây Logitech", price: 45, category: "Phụ kiện", inStock: true },
-{ id: "P03", name: "Bàn  phím cơ Keychron", price: 95, category: "Phụ kiện", inStock: false },
+{ id: "P02", name: "Chuột không dây Logitech", price: 45, category: "Phụ kiện", inStock: true },
+{ id: "P03", name: "Bàn phím cơ Keychron", price: 95, category: "Phụ kiện", inStock: false },
 { id: "P04", name: "Màn hinh Dell UltraSharp", price: 450, category: "Màn hinh", inStock: true },
 { id: "P05", name: "Tai nghe Sony WH-1000XM5", price: 350, category: "Phụ kiện", inStock: true }
 ];
@@ -41,8 +41,8 @@ const displayProduct=(itemArray)=>{
         }else{
             el.inStock="Hết hàng"
         }
-        return `${el.name} - Giá:$${el.price} - Trạng thái: ${el.inStock}`
-    })
+        return `${el.name} - Giá: $${el.price} - Trạng thái: ${el.inStock}`
+    }) 
     return catalogDisplay
 }
 console.log(displayProduct(products));
